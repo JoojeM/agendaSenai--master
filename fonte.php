4ad3878d93a4a104da5ee52330f753cdba0d9f36
Joao Miguel <joao.protachevicz@escola.pr.gov.br>
2022-12-14 20:25:11 -0300

    embelezamento2

--- fonte.php
+++ fonte.php
@@ -5,12 +5,20 @@
 p.a{
   font-family: cursive;
   font-size: 80px;
-
+  color: white;
 }
 
 p.b {
   font-family: cursive;
-  font-size: 20px
+  font-size: 20px;
+  color: white
+}
+
+p.c{
+  font-family: cursive;
+  font-size: 20px;
+  color: black;
+
 }
 </style>
 </head>
--- login.php
+++ login.php
@@ -22,12 +22,13 @@ if(!empty($_POST['email'])){
 </body>
 
     <head>
-    <h1><center><img src="img/logos_senai_branco.png" style="width: 35%" alt="Logo do Senai"></center></h1>
-
-    <h2><center><p class="a">LOGIN</center></h2>
 
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <h1><center><img src="img/logos_senai_branco.png" style="width: 35%" alt="Logo do Senai"></center></h1>
+
+    <h2><center><p class="a">LOGIN</center></h2>
 
 </head>
 <fieldset>
@@ -36,7 +37,7 @@ if(!empty($_POST['email'])){
         <input type="email" name="email"><br><br> </center>
        <center><p class="b"> Senha: <br>
         <input type="password" name="senha"><br><br></center>
-       <center><p class="b"> <input type="submit" value="Entrar"> </center>
+       <center><p class="c"> <input type="submit" value="Entrar"> </center>
         <link rel="styleheet" type="text/css" href="css/estilo.css">
     </form>
 </fieldset>
